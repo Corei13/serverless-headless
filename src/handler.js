6c107929fd8a38c843adff2dbe54ea45a128d8a9
Dhananjoy Biswas <ebappa1971@gmail.com>
2017-06-18 15:44:54 +0600

    Type louder, please.

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,8 @@ import Chrome from './chrome';
 export const run = async (event, context, callback) => {
   try {
     const chromePath = process.env.NODE_ENV === 'headless' ?
-      path.resolve(__dirname, './headless_shell') : undefined;
+      path.resolve(__dirname, './chrome/headless_shell') : undefined;
+
     const chrome = new Chrome({
       headless: process.env.NODE_ENV === 'headless',
       chromePath
